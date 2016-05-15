e2acf3b727924e71c1eaf8aa5cec0a913e780b03
KorJDH <jungeast92@gmail.com>
2016-05-15 21:32:34 +0900

    E03 Enemy 구현
    Enemy 구현 및
    총알이 Enemy에 닿았을때 Destroy 하게끔

---
+++ ShootingGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(NavMeshAgent)) ]
+public class Enemy : MonoBehaviour {
+
+    NavMeshAgent pathfinder;    //길찾기 관리
+    Transform target;
+
+	void Start () {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+	
+	void Update () {
+        //업데이트 때마다 경로지정을 하게되면 적이 많아질경우 값비싼 처리가된다. 
+        //타이머로 고정된 간격마다 하게 수정
+        //pathfinder.SetDestination(target.position);
+	}
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;  //반복 속도 1초
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
--- ShootingGame/Assets/Scripts/Projectile.cs
+++ ShootingGame/Assets/Scripts/Projectile.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+    public LayerMask collisionMask; //어떤 오브젝트, 어떤 레이어가 발사체와 충돌할지 결정할 수 있다
     float speed = 10;
 
     public void SetSpeed(float newSpeed)
@@ -11,6 +12,29 @@ public class Projectile : MonoBehaviour {
     }
 
 	void Update () {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
 	}
+
+    void CheckCollisions (float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit; //ray와 충돌한 오브젝트의 정보를 담기 위한 자료
+
+        //QueryTriggerInteraction 으로 우리가 트리거 콜라이더들과 충돌할지 안할지 정할 수 있음
+        if (Physics.Raycast(ray, out hit, moveDistance,collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);   //충돌
+        }
+
+    }
+
+    //충돌했을때 
+    void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
+
+    }
 }
